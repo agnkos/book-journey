983f296fba99073b23853d13c040d3e1294fae65
agnkos <101097155+agnkos@users.noreply.github.com>
2024-01-15 10:13:55 +0100

    css vars added

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
         Hello world!
       </h1>
       <div className="flex flex-col items-center gap-4">
-        <NavLink to='/login' className="px-4 py-2 bg-green-700 inline">Log In</NavLink>
+        <NavLink to='/login' className="px-4 py-2 bg-main-accent hover:bg-main-accent-hover inline">Log In</NavLink>
         <NavLink to='/signup' className="px-4 py-2 bg-green-700 inline">Sign In</NavLink>
       </div>
     </>
