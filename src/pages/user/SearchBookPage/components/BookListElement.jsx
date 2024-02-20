4fbc107198ce8f4508e06ca1cd9d6ee6315ee4e4
agnkos <101097155+agnkos@users.noreply.github.com>
2024-02-20 12:49:36 +0100

    search results displaying status if on the list

--- src/pages/user/SearchBookPage/components/BookListElement.jsx
+++ src/pages/user/SearchBookPage/components/BookListElement.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
-// import { Link } from 'react-router-dom';
 import { useContext, useState } from 'react';
+import { useLocation, Link } from "react-router-dom"
 import BookCoverPlaceholder from '../../../../components/BookCoverPlaceholder';
 import EditModal from '../../../../components/EditModal';
 import BookContext from '../../../../context/BookContext';
@@ -8,27 +8,14 @@ import BookContext from '../../../../context/BookContext';
 const BookListElement = ({ result }) => {
     const [showAddModal, setShowAddModal] = useState(false)
     const { books } = useContext(BookContext)
+    const location = useLocation()
 
     const closeAddModal = () => setShowAddModal(false)
     const openAddModal = () => setShowAddModal(true)
 
-    const isOnTheList = Object.values(books).flatMap(array => array.some(book => book.title === result.volumeInfo.title))
     const newList = Object.values(books).flatMap(array => array.map(book => book))
-    console.log('new list', newList)
     const filteredList = newList.filter(book => book.title === result.volumeInfo.title)
-    console.log('filtered list', filteredList)
-    // console.log('new list filter', newList.filter(book => book.title === result.volumeInfo.title))
-    // const filteredBooks2 = Object.values(books).filter(array => array.some(book => book.title === result.volumeInfo.title));
-    // console.log('filtered books', filteredBooks2)
 
-    // console.log('filter', Object.values(books).flatMap(array => array.filter(book => book.title === result.volumeInfo.title)))
-
-    // console.log('book title', Object.values(books).flatMap(array => array.map(book => console.log(book.title))))
-    // console.log('object values', Object.values(books).flatMap(array => array.map(book => book)))
-    // .filter(book => book.title === result.volumeInfo.title)
-    // console.log('result and book title', result.volumeInfo.title)
-    // console.log('isbn', result.volumeInfo.industryIdentifiers[0].identifier)
-    // console.log('all books', books)
     return (
         <>
             <div key={result.id} className="flex gap-4 my-4 max-w-[500px]">
@@ -45,15 +32,21 @@ const BookListElement = ({ result }) => {
 
                         <p className="font-semibold text-lg border-link-active">{result.volumeInfo.title}</p>
                         <p>{result.volumeInfo.authors}</p>
-                        {isOnTheList && <p>on the list</p>}
+                        {filteredList.length > 0 &&
+                            <p className='text-sm'>
+                                status: <span className='font-semibold'>{filteredList[0].status === "READ" ? 'read' : filteredList[0].status === "READING" ? 'reading' : 'to read'}</span>
+                            </p>}
                     </div>
-                    {/* <Link to="/addbook" state={{ author: result.volumeInfo.authors[0], title: result.volumeInfo.title }}
-                        className='mt-auto'
-                    > */}
-                    <button
-                        onClick={openAddModal}
-                        className="px-2 py-1 text-center bg-link-active hover:bg-link-active-hover text-light-bg rounded-md ml-auto block">Add to list</button>
-                    {/* </Link> */}
+                    {filteredList.length > 0 ?
+                        <Link to={`/books/${filteredList[0].id}`}
+                            state={{ path: location.pathname }}
+                        >
+                            <button className="px-2 py-1 text-center bg-link-active hover:bg-link-active-hover text-light-bg rounded-md ml-auto block">Details</button>
+                        </Link>
+                        :
+                        <button
+                            onClick={openAddModal}
+                            className="px-2 py-1 text-center bg-link-active hover:bg-link-active-hover text-light-bg rounded-md ml-auto block">Add to list</button>}
                 </div>
             </div>
             <hr className="bg-light-objects last:hidden max-w-[500px]" />
