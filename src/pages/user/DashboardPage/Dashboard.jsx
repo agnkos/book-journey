8e649ff7c7157321866a9aef7b802c5b8d33f7ba
agnkos <101097155+agnkos@users.noreply.github.com>
2024-03-14 16:02:00 +0100

    removed toast btn

--- src/pages/user/DashboardPage/Dashboard.jsx
+++ src/pages/user/DashboardPage/Dashboard.jsx
@@ -1,6 +1,5 @@
 import { useAuth } from "../../../hooks/useAuth";
 import ReadingNow from "./components/ReadingNow";
-import { toast } from 'react-toastify'
 
 const Dashboard = () => {
   const { user } = useAuth()
@@ -9,11 +8,6 @@ const Dashboard = () => {
     <div className="p-4">
       <h1 className="mb-2 text-2xl">Hello, <br /><span className="text-3xl font-semibold">{user?.username}</span></h1>
       <ReadingNow />
-      <button className="mt-6 px-4 py-2 text-center bg-lighter-accent hover:bg-main-accent text-light-bg rounded-md transition duration-15"
-        onClick={() => toast.success('Book added')}
-      >
-        toastify
-      </button>
     </div>
   )
 }
