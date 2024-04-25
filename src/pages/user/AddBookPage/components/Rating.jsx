a0edda1bae4a5481859a16ec1cf6d3f63359eca7
agnkos <101097155+agnkos@users.noreply.github.com>
2024-04-25 14:23:47 +0200

    range input outline

--- src/pages/user/AddBookPage/components/Rating.jsx
+++ src/pages/user/AddBookPage/components/Rating.jsx
@@ -8,16 +8,13 @@ const Rating = () => {
     const [hover, setHover] = useState(null)
 
     const setRating = (e) => {
-        console.log(e.currentTarget.id)
         setFieldValue('rate', e.currentTarget.id)
     }
 
     const starElements = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(num =>
     (<div key={num} >
         <StarIcon
-            className={`w-7 h-7 ${(hover || values.rate) >= num ? 'fill-link-active-hover' : 'fill-light-objects'} 
-      \
-            `}
+            className={`w-7 h-7 ${(hover || values.rate) >= num ? 'fill-link-active-hover' : 'fill-light-objects'}`}
             id={num}
             onClick={setRating}
             onMouseEnter={() => setHover(num)}
